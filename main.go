97fac5e389ea9509244d8f4875dc78b54fc88c21
Skye Bleed <mikeywasright@mailfence.com>
2019-05-10 13:14:46 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "bufio"
+  "os"
+  "strings"
+)
+
+func check_for_file(file string) bool {
+  if _, err := os.Stat(file); err == nil {
+    return true
+  } else {
+    return false
+  }
+}
+
+func read() string {
+  scanner := bufio.NewScanner(os.Stdin)
+  scanner.Scan()
+  return scanner.Text()
+}
+
+func main() {
+  fmt.Print("Enter the dir: ")
+  dir := read()
+  if check_for_file(dir + "/DESCRIPTION") {
+    content, _ := ioutil.ReadFile(dir + "/DESCRIPTION")
+    fmt.Printf("DESCRIPTION\n%s\n", content)
+    return
+  }
+
+  // check for readmes
+  files, err := ioutil.ReadDir("./" + dir)
+  if err != nil {
+      fmt.Printf("%v", err)
+  }
+
+  for _, f := range files {
+    if strings.HasPrefix(f.Name(), "README") {
+      content, _ := ioutil.ReadFile(dir + "/" + f.Name())
+      fmt.Printf("Found a README:\n%s\n", content)
+    }
+  }
+}
